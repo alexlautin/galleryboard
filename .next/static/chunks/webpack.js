2d367f890edae895230d710845abc53edab282e0
alexlautin <alexalautin@gmail.com>
2025-03-22 19:36:28 -0400

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "82cd0cb6ddb29e92"; }
+/******/ 		__webpack_require__.h = function() { return "5f643de3f50b92d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
